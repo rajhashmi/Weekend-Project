7d4cd74ba2f54cfbf3a67f1b2462000923a457d3
shahid <shahidhashmiraj@gmail.com>
2023-10-21 21:05:04 +0530

    addind more functionality

--- app.js
+++ app.js
@@ -1,44 +1,126 @@
 let data = [
-    { category: 'mobile', items: ["iPhone 12", 'Samsung Galaxy S21', 'Realme 8', 'Google Pixel 5', 'OnePlus 9'] },
-    { category: 'laptop', items: ['Infinix X1 Slim', 'MacBook Pro', 'HP Spectre x360', 'Dell XPS 13', 'Lenovo ThinkPad X1 Carbon'] },
-    { category: 'tablet', items: ['iPad Pro', 'Samsung Galaxy Tab S7', 'Amazon Fire HD', 'Microsoft Surface Go'] },
-    { category: 'smartwatch', items: ['Apple Watch Series 6', 'Samsung Galaxy Watch 4', 'Fitbit Versa 3', 'Garmin Venu'] },
-    { category: 'camera', items: ['Canon EOS R5', 'Sony Alpha A7III', 'Nikon Z6', 'Fujifilm X-T4'] },
-    { category: 'headphones', items: ['AirPods Pro', 'Sony WH-1000XM4', 'Bose QuietComfort 35 II', 'JBL Free X'] },
-    { category: 'homeAppliance', items: ['Roomba i7+', 'Nest Learning Thermostat', 'Instant Pot Duo', 'Dyson V11'] },
-    { category: 'gamingConsole', items: ['PlayStation 5', 'Xbox Series X', 'Nintendo Switch', 'PC Gaming Rig'] },
-    { category: 'kitchenAppliance', items: ['KitchenAid Stand Mixer', 'Vitamix Blender', 'Cuisinart Food Processor', 'Instant Pot Duo'] }
-  ];
-
-
-
+  {
+    category: "mobile",
+    id: 0,
+    items: [
+      "iPhone 12",
+      "Samsung Galaxy S21",
+      "Realme 8",
+      "Google Pixel 5",
+      "OnePlus 9",
+    ],
+  },
+  {
+    category: "laptop",
+    id: 1,
+    items: [
+      "Infinix X1 Slim",
+      "MacBook Pro",
+      "HP Spectre x360",
+      "Dell XPS 13",
+      "Lenovo ThinkPad X1 Carbon",
+    ],
+  },
+  {
+    category: "tablet",
+    id: 2,
+    items: [
+      "iPad Pro",
+      "Samsung Galaxy Tab S7",
+      "Amazon Fire HD",
+      "Microsoft Surface Go",
+    ],
+  },
+  {
+    category: "smartwatch",
+    id: 3,
+    items: [
+      "Apple Watch Series 6",
+      "Samsung Galaxy Watch 4",
+      "Fitbit Versa 3",
+      "Garmin Venu",
+    ],
+  },
+  {
+    category: "camera",
+    id: 4,
+    items: ["Canon EOS R5", "Sony Alpha A7III", "Nikon Z6", "Fujifilm X-T4"],
+  },
+  {
+    category: "headphones",
+    id: 5,
+    items: [
+      "AirPods Pro",
+      "Sony WH-1000XM4",
+      "Bose QuietComfort 35 II",
+      "JBL Free X",
+    ],
+  },
+  {
+    category: "homeAppliance",
+    id: 6,
+    items: [
+      "Roomba i7+",
+      "Nest Learning Thermostat",
+      "Instant Pot Duo",
+      "Dyson V11",
+    ],
+  },
+  {
+    category: "gamingConsole",
+    id: 7,
+    items: [
+      "PlayStation 5",
+      "Xbox Series X",
+      "Nintendo Switch",
+      "PC Gaming Rig",
+    ],
+  },
+  {
+    category: "kitchenAppliance",
+    id: 8,
+    items: [
+      "KitchenAid Stand Mixer",
+      "Vitamix Blender",
+      "Cuisinart Food Processor",
+      "Instant Pot Duo",
+    ],
+  },
+];
 const input = document.querySelector(".input");
 const showResult = document.querySelector(".unordedList");
+const Items = document.querySelector(".items");
 
-function clearPrevious(){
-    showResult.innerHTML = ''
+function clearPrevious(tagName) {
+  tagName.innerHTML = "";
 }
 
-function filterList(value){
-    let filter = data.filter((values)=>{
-       if(values.category.includes(value) && value !== ''){
-        return values
-       }
-    })
-    createList(filter)
+function filterList(value) {
+  let filter = data.filter((values) => {
+    return values.category.includes(value) && value !== "";
+  });
+  createList(filter);
 }
 
-function createList(filteredData){
-   filteredData.forEach((data)=>{
-   showResult.innerHTML = filteredData.map(element => `<li><a>${element.category}</a></li>`)
-   })
+function createList(filteredData) {
+  const listItems = filteredData.map((element) => `<li onclick="itemCatogory(${element.id})"><a>${element.category}</a></li>`).join("");
+  showResult.innerHTML = listItems;
 }
 
-input.addEventListener('input', (e)=>{
-    let inputValue = e.target.value
-    console.log(inputValue);
-    let value = inputValue.trim().toLowerCase();
-    clearPrevious()
-    filterList(value)
+function itemCatogory(index_) {
+  clearPrevious(Items);
+  clearPrevious(showResult);
+  const items = data[index_].items;
+  items.forEach((item) => {
+    let spans = document.createElement("span");
+    spans.textContent = item;
+    Items.appendChild(spans);
+  });
+}
 
-})
+input.addEventListener("input", (e) => {
+  let inputValue = e.target.value;
+  let value = inputValue.trim().toLowerCase();
+  clearPrevious(showResult);
+  filterList(value);
+});
